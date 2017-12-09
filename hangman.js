70b7104188579725097ab2b0348d71883f066148
Sorenious <30472130+Sorenious@users.noreply.github.com>
2017-12-09 10:31:11 -0700

    Added start over

--- hangman.js
+++ hangman.js
@@ -1,6 +1,6 @@
 var inquirer = require("inquirer");
-// playGame()
-// function playGame() {
+playGame()
+function playGame() {
 	function Word(word) {
 		this.wordList = ["bedrock", "biome", "block", "cobblestone", "crafting", "crafting table", "creeper", "diamond", "dungeon", "emerald", "the end", "ender dragon", "enderman", "farming", "furnace", "inventory", "lapis lazuli", "mining", "mobs", "mojang", "nether", "obsidian", "overworld", "pickaxe", "portal", "redstone", "skeleton", "smelting", "spawn point", "spider", "spider jockey", "stronghold", "temple", "tools", "witch", "wither", "zombie", "zombie pigmen"];
 		this.word = this.wordList[Math.floor(Math.random() * this.wordList.length)];
@@ -13,37 +13,21 @@ var inquirer = require("inquirer");
 	var blankWord= [];
 	var remainingGuesses = 9;
 	var gameOver = false;
-	//var testWord = wordList[Math.floor(Math.random() * wordList.length)];
 	var blankWordFill = "";
 	var alreadyGuessed;
 
 	// Converts the word to blank spaces so the player has to guess
 	for (var i = 0; i < testWord.word.length; i++) {
 			if (testWord.word[i] === " ") {
-				//console.log("D");
 				blankWord.push(" ");
 				blankWordFill += " " + blankWord[i] + " ";
 			} else {
-				//console.log("E");
 				blankWord.push("_");
 				blankWordFill += " " + blankWord[i] + " ";
 			}
 		}
 	guessAgain();
 	function guessAgain() {
-		// var wordList = ["bedrock", "biome", "block", "cobblestone", "crafting", "crafting table", "creeper", "diamond", "dungeon", "emerald", "the end", "ender dragon", "enderman", "farming", "furnace", "inventory", "lapis lazuli", "mining", "mobs", "mojang", "nether", "obsidian", "overworld", "pickaxe", "portal", "redstone", "skeleton", "smelting", "spawn point", "spider", "spider jockey", "stronghold", "temple", "tools", "witch", "wither", "zombie", "zombie pigmen"];
-		// var wrongGuessImgs = ["assets/images/half-creeper.png", "assets/images/full-creeper.png", "assets/images/full-creeper-flash.png", "assets/images/full-creeper-flash.png", "assets/images/full-creeper-flash.png", "assets/images/full-creeper-flash.png"]
-		
-		//var instruction = document.getElementById("message");
-
-		//instruction.innerHTML = ("");
-
-		//blankWordFill = "";
-		// $("#letters-guessed").text("");
-		// $("#creeper").attr("src", "");
-
-		
-		
 		console.log("Remaining number of guesses: "+remainingGuesses);
 		console.log(blankWordFill+"\n");
 
@@ -51,7 +35,6 @@ var inquirer = require("inquirer");
 		function previousGuesses() {
 			if (guesses.indexOf(userGuess) < 0) {
 				guesses.push(userGuess);
-				//$("#letters-guessed").append(" " + userGuess.toUpperCase() + " ");
 				alreadyGuessed = guesses[0].toUpperCase();
 				for (var i = 1; i < guesses.length; i++) {
 					alreadyGuessed += ", " + guesses[i].toUpperCase();
@@ -65,20 +48,12 @@ var inquirer = require("inquirer");
 			if (testWord.word.indexOf(userGuess) < 0) { // Letter is not in the word(s)
 				if (guesses.indexOf(userGuess) < 0) { // If letter already guessed, skip this(do nothing)
 					wrongGuessNum += 1;
-					// $("#creeper").attr("src", wrongGuessImgs[wrongGuessNum]);
-					// if (wrongGuessNum >= 1 && wrongGuessNum < (wrongGuessImgs.length-1)) {
-					//   setTimeout(function() {
-					//     $("#creeper").attr("src", "assets/images/full-creeper.png");
-					//   }, 1000);
-					// }
 					remainingGuesses -= 1;
+					console.log("INCORRECT!\n");
 				}
 				if (remainingGuesses === 0) {
 					gameOver = true;
 				}
-				previousGuesses();
-				guessAgain();
-				//$("#numGuessRemain").text(remainingGuesses);
 			} else {
 				for (var l = 0; l<testWord.word.length; l++) {
 					if (testWord.word[l] === userGuess) {
@@ -98,26 +73,12 @@ var inquirer = require("inquirer");
 				if (blankWord.indexOf("_") < 0) {
 					gameOver = true;
 				}
-				previousGuesses();
-				guessAgain();
-				// if (wrongGuessNum >= 1) {
-				//   $("#creeper").attr("src", "assets/images/hurt-creeper.png");
-				//   if (!(blankWord.indexOf("_") < 0)) {
-				//     setTimeout(function() {
-				//       $("#creeper").attr("src", "assets/images/full-creeper.png");
-				//     }, 500);
-				//   }
-				// }
+				console.log("CORRECT!\n")
+				
 			}
-	      
+			previousGuesses();
+			guessAgain();
 	    };
-
-	    // function gameOver() {
-	    // 	if (blankWord.indexOf("_") < 0) {
-
-	    // 	}
-	    // };
-	    //console.log(gameOver);
 		if (!gameOver) {
 			inquirer.prompt([
 				{
@@ -135,57 +96,11 @@ var inquirer = require("inquirer");
 				userTakesAGuess();
 			});
 	    }
-	        //}
-	      
-	      // if (blankWord.indexOf("_") < 0) {
-	      //   console.log("YOU WIN!!");
-	      //   // if (!gameOver && wrongGuessNum >= 1) {
-	      //   //   $("#creeper").attr("src", "assets/images/dead-creeper.png");
-	      //   //   setTimeout(function() {
-	      //   //     $("#creeper").attr("src", "assets/images/gunpowder.png");
-	      //   //   }, 1000);
-	      //   // }
-	      //   gameOver = true;
-	      //   return;
-	      //   // if (userGuess === " ") {
-	      //   //   playGame();
-	      //   // }
-	      // } else if (remainingGuesses === 0) {
-	      //   blankWordFill = "";
-	      //   for (var i = 0; i < testWord.word.length; i++) {
-	      //     if (testWord.word[i] === " ") {
-	      //       blankWordFill += "   ";
-	      //     } else {
-	      //       blankWordFill += " " + testWord.word[i].toUpperCase() + " ";
-	      //     }
-	      //   }
-	      //   console.log("You lose. :(");
-	      //   // if (!gameOver) {
-	      //   //   var audioElement = document.createElement("audio");
-	      //   //   audioElement.setAttribute("src", "assets/sounds/creeper-sound.mp3");
-	      //   //   audioElement.play();
-	      //   //   setTimeout(function() {
-	      //   //     $("#creeper").attr("src", "assets/images/full-creeper.png");
-	      //   //     setTimeout(function() {
-	      //   //       $("#creeper").attr("src", "assets/images/full-creeper-flash.png");
-	      //   //       setTimeout(function() {
-	      //   //         $("#creeper").attr("src", "assets/images/full-creeper.png");
-	      //   //         setTimeout(function() {
-	      //   //           $("#creeper").attr("src", "assets/images/creeper-explode.png");
-	      //   //         }, 900);
-	      //   //       }, 900);
-	      //   //     }, 900);
-	      //   //   }, 900);
-	      //   // }
-	      //   gameOver = true;
-	      //   return;
-	      //   // if (userGuess === " ") {
-	      //   //   playGame();
-	      //   // }
-	      // }
 
 		if (gameOver && blankWord.indexOf("_") < 0) {
 			console.log("YOU WIN!!");
+			console.log("Next word!\n");
+			playGame();
 		} else if (gameOver && remainingGuesses === 0) {
 			blankWordFill = "";
 			for (var i = 0; i < testWord.word.length; i++) {
@@ -196,6 +111,8 @@ var inquirer = require("inquirer");
 				}
 			}
 			console.log("You lose. :(");
+			console.log("Next word!\n");
+			playGame();
 		}
 	};
-//};
+};
